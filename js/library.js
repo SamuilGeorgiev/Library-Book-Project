779c5ebf5912395e52f3596c6a68719d86cb2f07
Samuil <samuil0240@gmail.com>
2023-02-24 12:43:23 +0200

    Added library.js file with functions to manage books

---
+++ js/library.js
@@ -0,0 +1,14 @@
+/**
+ * This function gets the list of books from local storage
+ */
+function getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+    return books;
+  }
+
+  
